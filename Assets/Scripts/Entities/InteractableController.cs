a8190180ea168b4951d19a6e11d958c3c165bf92
BiscuitPrime <henri.nomico@gmail.com>
2023-07-08 12:30:36 +0200

    Added base for interactable objects

---
+++ Assets/Scripts/Entities/InteractableController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inverted.Entities
+{
+    /// <summary>
+    /// Script used by the elements that can be moved and controlled through the player's mouse.
+    /// </summary>
+    public class InteractableController : MonoBehaviour
+    {
+    
+    }
+}
