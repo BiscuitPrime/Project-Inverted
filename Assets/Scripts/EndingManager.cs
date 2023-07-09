83d0065615a1a2cccd906808334ad1a057701ece
BiscuitPrime <henri.nomico@gmail.com>
2023-07-09 19:33:26 +0200

    Polished finished

--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndingManager : MonoBehaviour
+namespace Inverted.Levels
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EndingManager : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            GameManager.Instance.TriggerEnding();
+        }
     }
 }
